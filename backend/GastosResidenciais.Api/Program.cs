f38dc51c57fd38bdfea74cbb0254c1421164d99f
douglassilvadev <gh_DEi28mJJd3PPcyC1Nbq6OcUmNo3UKO26HgBs>
2026-03-12 23:39:05 -0300

    chore: finalize docker setup with automatic migrations

--- backend/GastosResidenciais.Api/Program.cs
+++ backend/GastosResidenciais.Api/Program.cs
@@ -60,6 +60,12 @@ builder.Services.AddScoped<ConsultarTotaisPorCategoriaUseCase>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
